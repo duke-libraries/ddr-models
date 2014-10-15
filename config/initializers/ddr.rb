9f4ab530553cad9e1fc85ec9738f68d473b71376
Jim Coble <jim.coble@duke.edu>
2014-10-15 16:32:58 -0400

    wip

--- config/initializers/ddr.rb
+++ config/initializers/ddr.rb
@@ -1,6 +1,6 @@
 Ddr::Models.configure do |config|
 
-  config.external_file_store = Rails.env.test? ? "/tmp" : ENV['EXTERNAL_FILE_STORE']
+  config.external_file_store = ENV['EXTERNAL_FILE_STORE']
   config.external_file_subpath_pattern = Rails.env.test? ? "--" : ENV['EXTERNAL_FILE_SUBPATH_PATTERN']
   config.noid_template = "2.reeddeeddk"
   config.minter_statefile = Rails.env.test? ? "/tmp/minter-state" : ENV['MINTER_STATEFILE']
--- lib/ddr/models/version.rb
+++ lib/ddr/models/version.rb
@@ -1,5 +1,5 @@
 module Ddr
   module Models
-    VERSION = "0.1.9"
+    VERSION = "0.1.10"
   end
 end
