7d1d934ed5476cc356150632d11f3e2327a58902
Jim Coble <jim.coble@duke.edu>
2014-11-07 09:32:11 -0500

    Conditionally add columns to users table; closes #34

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104181418) do
+ActiveRecord::Schema.define(version: 20141107124012) do
 
   create_table "events", force: true do |t|
     t.datetime "event_date_time"
