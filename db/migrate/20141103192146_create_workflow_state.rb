cbcf67112472bdef882e361a362f0d949d008fc6
Jim Coble <jim.coble@duke.edu>
2014-11-07 11:45:27 -0500

    change workflow_states table creation so that it is conditional on table not already existing; closes #35

--- db/migrate/20141103192146_create_workflow_state.rb
+++ db/migrate/20141103192146_create_workflow_state.rb
@@ -1,13 +1,19 @@
 class CreateWorkflowState < ActiveRecord::Migration
-  def change
-    create_table "workflow_states" do |t|
-      t.string   "pid"
-      t.string   "workflow_state"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-    end
+  def up
+    unless table_exists?("workflow_states")
+      create_table "workflow_states" do |t|
+        t.string   "pid"
+        t.string   "workflow_state"
+        t.datetime "created_at"
+        t.datetime "updated_at"
+      end
 
-    add_index "workflow_states", ["pid"], name: "index_workflow_states_on_pid", unique: true
-    add_index "workflow_states", ["workflow_state"], name: "index_workflow_states_on_workflow_state"
+      add_index "workflow_states", ["pid"], name: "index_workflow_states_on_pid", unique: true
+      add_index "workflow_states", ["workflow_state"], name: "index_workflow_states_on_workflow_state"
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
