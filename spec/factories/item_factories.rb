173265fe6d86a01ade85bc37514e7300aa220ffd
dchandekstark <dchandekstark@gmail.com>
2014-11-10 11:14:34 -0500

    Removed unused factory traits (fixes #25)

--- spec/factories/item_factories.rb
+++ spec/factories/item_factories.rb
@@ -3,14 +3,6 @@ FactoryGirl.define do
   factory :item do
     title [ "Test Item" ]
     sequence(:identifier) { |n| [ "item%05d" % n ] }
-
-    trait :member_of_collection do
-      collection
-    end
-    
-    trait :has_part do
-      children { [ FactoryGirl.create(:component) ] }
-    end
   end
 
 end
