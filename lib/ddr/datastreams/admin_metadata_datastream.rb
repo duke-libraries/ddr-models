f153639a6adbdb2ad1fcfd37951eda1b5655da79
dchandekstark <dchandekstark@gmail.com>
2015-01-09 09:16:36 -0500

    original_filename now stored in adminMetadata using premis:hasOriginalName predicate. Closes #85 This change clears the way to remove the properties datastream and may help with Fedora 4 migration since Fedora 4 stores the original name of a binary file under the same predicate.

--- lib/ddr/datastreams/admin_metadata_datastream.rb
+++ lib/ddr/datastreams/admin_metadata_datastream.rb
@@ -1,10 +1,12 @@
+require "rdf-vocab"
+
 module Ddr
   module Datastreams
     class AdminMetadataDatastream < ActiveFedora::NtriplesRDFDatastream
 
       property :permanent_id, predicate: Ddr::Vocab::Asset.permanentId
-
       property :permanent_url, predicate: Ddr::Vocab::Asset.permanentUrl
+      property :original_filename, predicate: RDF::Vocab::PREMIS::V1.hasOriginalName
 
       Ddr::Vocab::Roles.each do |term|
         property Ddr::Metadata::Vocabulary.term_name(Ddr::Vocab::Roles, term), 
