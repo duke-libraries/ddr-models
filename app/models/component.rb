add00c34380a3b8bc7643d155e86cf97776b4796
Jim Coble <jim.coble@duke.edu>
2015-02-23 15:19:11 -0500

    retrieve item children grouped by file_use and sorted by order within each file_use group; closes #132

--- app/models/component.rb
+++ app/models/component.rb
@@ -6,6 +6,7 @@
 class Component < Ddr::Models::Base
  
   include Ddr::Models::HasContent
+  include Ddr::Models::HasStructMetadata
 
   belongs_to :parent, :property => :is_part_of, :class_name => 'Item'
   belongs_to :target, :property => :has_external_target, :class_name => 'Target'
