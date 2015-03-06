18e21015f8f99111b0857e37bf2912e78b7d8469
Jim Coble <jim.coble@duke.edu>
2015-03-06 13:23:30 -0500

    index license fields in permissions and default permissions; closes #155

--- spec/support/shared_examples_for_ddr_models.rb
+++ spec/support/shared_examples_for_ddr_models.rb
@@ -1,6 +1,7 @@
 RSpec.shared_examples "a DDR model" do
   it_behaves_like "a describable object"
   it_behaves_like "a governable object"
+  it_behaves_like "a licensable object"
   it_behaves_like "an access controllable object"
   it_behaves_like "an object that has properties"
   it_behaves_like "an object that has a display title"
