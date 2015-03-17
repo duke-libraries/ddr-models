c852a38c3e06fe21ad209a47bf84895239972c5d
dchandekstark <dchandekstark@gmail.com>
2015-03-17 10:50:47 -0400

    Ddr::Models::HasChildren#first_child returns nil if object has no children
    Fixes #165
    Also uses ActiveFedora query methods instead of SolrService and find.
    The conditional branch which uses the deprecated contentMetadata
    datastream is NOT tested, and was left in only for compatibility
    with dul-hydra.

--- lib/ddr/models/has_children.rb
+++ lib/ddr/models/has_children.rb
@@ -1,19 +1,15 @@
 module Ddr
   module Models
     module HasChildren
-      extend ActiveSupport::Concern
 
       def first_child
         if datastreams.include?(Ddr::Datastreams::CONTENT_METADATA) && datastreams[Ddr::Datastreams::CONTENT_METADATA].has_content?
+          # DEPRECATED
           first_child_pid = datastreams[Ddr::Datastreams::CONTENT_METADATA].first_pid
+          first_child_pid ? ActiveFedora::Base.find(first_child_pid) : nil
         else
-          first_child_pid = ActiveFedora::SolrService.query(association_query(:children), rows: 1, sort: "#{Ddr::IndexFields::IDENTIFIER} ASC").first["id"]
+          ActiveFedora::Base.where(association_query(:children)).order("#{Ddr::IndexFields::IDENTIFIER} ASC").first
         end      
-        begin
-          ActiveFedora::Base.find(first_child_pid, :cast => true) if first_child_pid
-        rescue ActiveFedora::ObjectNotFound
-          nil
-        end
       end
 
     end
--- lib/ddr/models/version.rb
+++ lib/ddr/models/version.rb
@@ -1,5 +1,5 @@
 module Ddr
   module Models
-    VERSION = "1.11.8"
+    VERSION = "1.11.9"
   end
 end
