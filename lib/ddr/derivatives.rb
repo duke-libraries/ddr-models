3a296df918543ee3795e0511daa11d671b4ac87d
Jim Coble <jim.coble@duke.edu>
2015-05-04 16:57:13 -0400

    temporarily disable ptif generation

--- lib/ddr/derivatives.rb
+++ lib/ddr/derivatives.rb
@@ -10,11 +10,12 @@ module Ddr
 
     # Eventually, we should inject the generator (probably) and the options (certainly) for each derivative
     # (e.g., from configuration)
-    DERIVATIVES = { multires_image:
-                        Derivative.new( :multires_image,
-                                        Ddr::Datastreams::MULTIRES_IMAGE,
-                                        Ddr::Derivatives::PtifGenerator,
-                                        "jpeg:90,tile:256x256,pyramid"),
+    DERIVATIVES = {
+                    # multires_image:
+                    #     Derivative.new( :multires_image,
+                    #                     Ddr::Datastreams::MULTIRES_IMAGE,
+                    #                     Ddr::Derivatives::PtifGenerator,
+                    #                     "jpeg:90,tile:256x256,pyramid"),
                     thumbnail:
                         Derivative.new( :thumbnail,
                                         Ddr::Datastreams::THUMBNAIL,
