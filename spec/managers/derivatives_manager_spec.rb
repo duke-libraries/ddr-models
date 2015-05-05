bc8fd8f92dfed95aeacc27aebabd5f4c1f073284
Jim Coble <jim.coble@duke.edu>
2015-05-05 09:01:53 -0400

    Add extension to output file name passed to generators; fixes #230.
    Also add tests for generators and improve derivatives manager tests.

--- spec/managers/derivatives_manager_spec.rb
+++ spec/managers/derivatives_manager_spec.rb
@@ -12,14 +12,14 @@ module Ddr
           include Ddr::Models::EventLoggable
           _save_callbacks.clear
         end
-        # class MultiresImageable < ContentBearing
-        #   include Ddr::Models::HasMultiresImage
-        #   _save_callbacks.clear
-        # end
+        class MultiresImageable < ContentBearing
+          include Ddr::Models::HasMultiresImage
+          _save_callbacks.clear
+        end
       end
 
       after(:all) do
-        # Ddr::Managers.send(:remove_const, :MultiresImageable)
+        Ddr::Managers.send(:remove_const, :MultiresImageable)
         Ddr::Managers.send(:remove_const, :ContentBearing)
       end
 
@@ -28,10 +28,10 @@ module Ddr
         context "not multires_image_able" do
           let(:object) { ContentBearing.new }
           context "content is image" do
-            let(:file) { fixture_file_upload("image1.tiff", "image/tiff") }
+            let(:file) { fixture_file_upload("imageA.tif", "image/tiff") }
             it "should generate a thumbnail and not a ptif" do
               expect(object.derivatives).to receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:thumbnail])
-              # expect(object.derivatives).to_not receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:multires_image])
+              expect(object.derivatives).to_not receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:multires_image])
               object.derivatives.update_derivatives(:now)
             end
           end
@@ -39,7 +39,7 @@ module Ddr
             let(:file) { fixture_file_upload("sample.pdf", "application/pdf") }
             it "should generate a thumbnail and not a ptif" do
               expect(object.derivatives).to receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:thumbnail])
-              # expect(object.derivatives).to_not receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:multires_image])
+              expect(object.derivatives).to_not receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:multires_image])
               object.derivatives.update_derivatives(:now)
             end
           end
@@ -47,34 +47,34 @@ module Ddr
             let(:file) { fixture_file_upload("sample.docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document") }
             it "should generate neither a thumbnail nor a ptif" do
               expect(object.derivatives).to_not receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:thumbnail])
-              # expect(object.derivatives).to_not receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:multires_image])
+              expect(object.derivatives).to_not receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:multires_image])
               object.derivatives.update_derivatives(:now)
             end
           end
         end
-        # context "multires_image_able" do
-        #   let(:object) { MultiresImageable.new }
-        #   context "content is tiff image" do
-        #     let(:file) { fixture_file_upload("image1.tiff", "image/tiff") }
-        #     it "should generate a thumbnail and a ptif" do
-        #       expect(object.derivatives).to receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:thumbnail])
-        #       expect(object.derivatives).to receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:multires_image])
-        #       object.derivatives.update_derivatives(:now)
-        #     end
-        #   end
-        #   context "content is not tiff image" do
-        #     let(:file) { fixture_file_upload("sample.pdf", "application/pdf") }
-        #     it "should generate a thumbnail but not a ptif" do
-        #       expect(object.derivatives).to receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:thumbnail])
-        #       expect(object.derivatives).to_not receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:multires_image])
-        #       object.derivatives.update_derivatives(:now)
-        #     end
-        #   end
-        # end
+        context "multires_image_able" do
+          let(:object) { MultiresImageable.new }
+          context "content is tiff image" do
+            let(:file) { fixture_file_upload("imageA.tif", "image/tiff") }
+            it "should generate a thumbnail and a ptif" do
+              expect(object.derivatives).to receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:thumbnail])
+              expect(object.derivatives).to receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:multires_image])
+              object.derivatives.update_derivatives(:now)
+            end
+          end
+          context "content is not tiff image" do
+            let(:file) { fixture_file_upload("sample.pdf", "application/pdf") }
+            it "should generate a thumbnail but not a ptif" do
+              expect(object.derivatives).to receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:thumbnail])
+              expect(object.derivatives).to_not receive(:generate_derivative!).with(Ddr::Derivatives::DERIVATIVES[:multires_image])
+              object.derivatives.update_derivatives(:now)
+            end
+          end
+        end
       end
 
       describe "derivative generation" do
-        let(:file) { fixture_file_upload("image1.tiff", "image/tiff") }
+        let(:file) { fixture_file_upload("imageA.tif", "image/tiff") }
         before { object.upload! file }
         describe "thumbnail" do
           let(:object) { ContentBearing.create }
@@ -82,26 +82,27 @@ module Ddr
             expect(object.datastreams[Ddr::Datastreams::THUMBNAIL]).to_not be_present
             object.derivatives.generate_derivative! Ddr::Derivatives::DERIVATIVES[:thumbnail]
             expect(object.datastreams[Ddr::Datastreams::THUMBNAIL]).to be_present
+            expect(object.datastreams[Ddr::Datastreams::THUMBNAIL].size).to be > 0
           end
         end
-        # describe "ptif" do
-        #   let(:object) { MultiresImageable.create }
-        #   it "should create content in the multires image datastream" do
-        #     expect(object.datastreams[Ddr::Datastreams::MULTIRES_IMAGE]).to_not be_present
-        #     object.derivatives.generate_derivative! Ddr::Derivatives::DERIVATIVES[:multires_image]
-        #     expect(object.datastreams[Ddr::Datastreams::MULTIRES_IMAGE]).to be_present
-        #   end
-        # end
+        describe "ptif" do
+          let(:object) { MultiresImageable.create }
+          it "should create content in the multires image datastream" do
+            expect(object.datastreams[Ddr::Datastreams::MULTIRES_IMAGE]).to_not be_present
+            object.derivatives.generate_derivative! Ddr::Derivatives::DERIVATIVES[:multires_image]
+            expect(object.datastreams[Ddr::Datastreams::MULTIRES_IMAGE]).to be_present
+            file_uri = object.datastreams[Ddr::Datastreams::MULTIRES_IMAGE].dsLocation
+            expect(File.size(Ddr::Utils.path_from_uri(file_uri))).to be > 0
+          end
+        end
       end
 
       describe "event creation" do
-        let(:file) { fixture_file_upload("image1.tiff", "image/tiff") }
+        let(:file) { fixture_file_upload("imageA.tif", "image/tiff") }
         before { object.upload! file }
-        # let(:object) { MultiresImageable.create }
-        let(:object) { ContentBearing.create }
+        let(:object) { MultiresImageable.create }
         it "should create an update event for each derivative updated" do
-          # expect {object.derivatives.update_derivatives(:now) }.to change { object.update_events.count }.by(2)
-          expect {object.derivatives.update_derivatives(:now) }.to change { object.update_events.count }.by(1)
+          expect {object.derivatives.update_derivatives(:now) }.to change { object.update_events.count }.by(2)
         end
       end
 
