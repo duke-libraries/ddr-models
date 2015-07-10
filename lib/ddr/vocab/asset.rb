484455e8fbb4f2ddbc68011a15abe0c15b83c6ba
Jim Coble <jim.coble@duke.edu>
2015-07-10 10:37:52 -0400

    Remove references old structural metadata attributes; closes #256.
    Remove FILE_USE, ORDER, FILE_GROUP from Ddr::IndexFields.
    Remove fileUse, order, fileGroup properties from Ddr::Vocab::Asset.

--- lib/ddr/vocab/asset.rb
+++ lib/ddr/vocab/asset.rb
@@ -2,20 +2,11 @@
   module Vocab
     class Asset < RDF::StrictVocabulary("http://repository.lib.duke.edu/vocab/asset/")
 
-      property "order",
-        label: "Order"
-
       property "permanentId",
         label: "Permanent Identifier"
 
       property "permanentUrl",
         label: "Permanent URL"
-
-      property "fileGroup",
-        label: "File Group"
-
-      property "fileUse",
-        label: "File Use"
 
       property "workflowState",
         label: "Workflow State"
