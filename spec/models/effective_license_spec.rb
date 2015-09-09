9c827cdac6bc0e0e5e6e2948440d3e28b4673f0f
David Chandek-Stark <dchandekstark@gmail.com>
2015-09-08 22:02:22 -0400

    Adds support for retrieving list of all licenses.
    Removes internal ddr-aux API client in favor of
    ddr-aux-client.

--- spec/models/effective_license_spec.rb
+++ spec/models/effective_license_spec.rb
@@ -3,15 +3,15 @@ module Ddr::Models
 
     subject { described_class.call(obj) }
 
-    let(:mock) { Struct.new(:license, :parent, :admin_policy) }
+    let(:mock) { Struct.new(:license, :parent, :admin_policy, :pid) }
     let(:obj) { mock.new }
 
-    let(:code) { "cc-by-nc-40" }
-    let(:license) { Ddr::Models::License.new(code: code) }
-    before { allow(Ddr::Models::License).to receive(:get).with(code) { license } }
+    let(:url) { "https://creativecommons.org/licenses/by-nc-nd/4.0/" }
+    let(:license) { License.new(url: url) }
+    before { allow(License).to receive(:get).with(url) { license } }
 
-    describe "when the object has a licence code" do
-      before { obj.license = code }
+    describe "when the object has a licence" do
+      before { obj.license = url }
       it { is_expected.to eq(license) }
     end
 
@@ -19,7 +19,7 @@ module Ddr::Models
       describe "when the object has a parent" do
         let(:parent) { mock.new }
         before do
-          parent.license = code
+          parent.license = url
           obj.parent = parent
         end
         it { is_expected.to eq(license) }
@@ -29,7 +29,7 @@ module Ddr::Models
           describe "and the admin policy is a different object" do
             let(:admin_policy) { mock.new }
             before do
-              admin_policy.license = code
+              admin_policy.license = url
               obj.admin_policy = admin_policy
             end
             it { is_expected.to eq(license) }
