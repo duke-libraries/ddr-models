1fbd5ca2f803f328219a22b084428fc8d72bab0f
David Chandek-Stark <dchandekstark@gmail.com>
2015-10-07 21:47:21 -0400

    Revises FindingAid fields per #402.
    Closes #402

--- lib/ddr/models/finding_aid.rb
+++ lib/ddr/models/finding_aid.rb
@@ -1,21 +1,54 @@
+require "nokogiri"
+
 module Ddr::Models
   class FindingAid
 
     attr_reader :ead_id
+
+    EAD_XMLNS = "urn:isbn:1-931666-22-9"
 
     def initialize(ead_id)
       @ead_id = ead_id
     end
 
     def url
-      @url ||= doc.css("eadid").attr("url").text
+      doc.css("eadid").attr("url").text
     end
 
+    # The finding aid title
     def title
-      @title ||= doc.css("titleproper").children.first.text.strip
+      doc.css("titleproper").children.first.text.strip
+    end
+
+    def repository
+      collection.xpath('ead:did/ead:repository/ead:corpname', ead: EAD_XMLNS).text
+    end
+
+    def collection_date_span
+      collection.xpath('ead:did/ead:unitdate[@type="inclusive"]', ead: EAD_XMLNS).text
+    end
+
+    def collection_number
+      collection.xpath('ead:did/ead:unitid', ead: EAD_XMLNS).text
+    end
+
+    def collection_title
+      collection.xpath('ead:did/ead:unittitle', ead: EAD_XMLNS).text
+    end
+
+    def extent
+      collection.xpath('ead:did/ead:physdesc/ead:extent', ead: EAD_XMLNS).map(&:text).join("; ")
+    end
+
+    def abstract
+      collection.xpath('ead:did/ead:abstract', ead: EAD_XMLNS).text
     end
 
     private
+
+    def collection
+      doc.xpath('//ead:archdesc[@level="collection"]', ead: EAD_XMLNS)
+    end
 
     # @raise [OpenURI::HTTPError] if 404, etc.
     def doc
--- spec/models/finding_aid_spec.rb
+++ spec/models/finding_aid_spec.rb
@@ -15,15 +15,32 @@ module Ddr::Models
               findaidstatus="published"
               langencoding="iso639-2b"
               repositoryencoding="iso15511">
-      <eadid url="http://example.com/ead/">ead</eadid>
-      <filedesc>
-         <titlestmt>
-            <titleproper>Guide to the Perplexed
-              <num>00001</num>
-            </titleproper>
-          </titlestmt>
-      </filedesc>
+     <eadid url="http://example.com/ead/">ead</eadid>
+     <filedesc>
+       <titlestmt>
+         <titleproper>Guide to the Perplexed
+           <num>00001</num>
+         </titleproper>
+       </titlestmt>
+     </filedesc>
    </eadheader>
+   <archdesc level="collection">
+     <did>
+       <repository>
+         <corpname>Library of the Perplexed</corpname>
+       </repository>
+       <unittitle>Perplexities</unittitle>
+       <unitid>RL.00327</unitid>
+       <physdesc altrender="whole">
+         <extent altrender="materialtype spaceoccupied">6.5 Linear Feet</extent>
+       </physdesc>
+       <unitdate normal="1876/1953" type="inclusive">1876-1953</unitdate>
+       <abstract>Abstract of Perplexities.</abstract>
+       <physdesc id="aspace_foobar">
+         <extent>4000 Items</extent>
+       </physdesc>
+     </did>
+   </archdesc>
 </ead>
 EOS
     end
@@ -34,6 +51,12 @@ EOS
 
     its(:title) { is_expected.to eq("Guide to the Perplexed") }
     its(:url) { is_expected.to eq("http://example.com/ead/") }
+    its(:repository) { is_expected.to eq("Library of the Perplexed") }
+    its(:collection_date_span) { is_expected.to eq("1876-1953") }
+    its(:collection_number) { is_expected.to eq("RL.00327") }
+    its(:collection_title) { is_expected.to eq("Perplexities") }
+    its(:extent) { is_expected.to eq("6.5 Linear Feet; 4000 Items") }
+    its(:abstract) { is_expected.to eq("Abstract of Perplexities.") }
 
   end
 end
