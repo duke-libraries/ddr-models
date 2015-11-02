bc4176aa1a52dc74d962a686dcdafc63a7dcc6db
David Chandek-Stark <dchandekstark@gmail.com>
2015-11-02 16:50:56 -0500

    Patched HasAttachments to that inverse rel is `attached_to`.

--- lib/ddr/models/has_attachments.rb
+++ lib/ddr/models/has_attachments.rb
@@ -6,7 +6,8 @@ module Ddr
       included do
         has_many :attachments,
                  predicate: ::RDF::URI("http://projecthydra.org/ns/relations#isAttachedTo"),
-                 class_name: 'Attachment'
+                 class_name: 'Attachment',
+                 as: :attached_to
       end
 
     end
