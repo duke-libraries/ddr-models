5d6213e3860d70c4e8425c94e59cf16762b1fd90
David Chandek-Stark <dchandekstark@gmail.com>
2015-12-09 16:28:07 -0500

    Use Deprecation gem (closes #483)

--- lib/ddr/models/indexing.rb
+++ lib/ddr/models/indexing.rb
@@ -4,6 +4,10 @@ module Ddr
 
       include Ddr::Index::Fields
 
+      def self.const_missing(name)
+        Ddr::Index::Fields.const_missing(name)
+      end
+
       def to_solr(solr_doc=Hash.new, opts={})
         solr_doc = super(solr_doc, opts)
         solr_doc.merge index_fields
