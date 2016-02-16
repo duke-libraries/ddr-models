6748af6e8b5df08cae9787f799ca0f44578166b1
Jim Coble <jim.coble@duke.edu>
2016-02-16 11:18:24 -0500

    Deprecate obsolete method; closes #547.

--- app/helpers/models_helper.rb
+++ app/helpers/models_helper.rb
@@ -1,6 +1,9 @@
 module ModelsHelper
+  extend Deprecation
 
   def admin_set_full_name(slug)
+    Deprecation.warn(self, "admin_set_full_name helper method is deprecated and will be removed in v3." \
+                           " Retrieve admin set title from AdminSet ActiveResource instead.")
     I18n.t("ddr.admin_set.#{slug}")
   end
 
