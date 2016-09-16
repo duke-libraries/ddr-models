18bd0444f73f62489e1f5e48c123d8027491a838
David Chandek-Stark <dchandekstark@gmail.com>
2016-09-16 16:00:31 -0400

    Adds :export ability to Collection
    The ability is granted if the auth context agents have a policy scope role
    that grants the :read permission.
    
    Version 2.4.15

--- lib/ddr/auth/ability_definitions/collection_ability_definitions.rb
+++ lib/ddr/auth/ability_definitions/collection_ability_definitions.rb
@@ -6,6 +6,22 @@ module Ddr
         if member_of? Ddr::Auth.collection_creators_group
           can :create, ::Collection
         end
+        can :export, ::Collection do |obj|
+          has_policy_permission?(obj, Permissions::READ)
+        end
+      end
+
+      private
+
+      def policy_permissions(obj)
+        obj.roles
+          .in_policy_scope
+          .agent(agents)
+          .permissions
+      end
+
+      def has_policy_permission?(obj, perm)
+        policy_permissions(obj).include?(perm)
       end
 
     end
