bb38d890c0ecf8bd097c0b0f36e9b2e98c1c8b1d
David Chandek-Stark <david.chandek.stark@duke.edu>
2016-10-20 21:28:10 -0400

    Removes old hack for Hydra permissions

--- lib/ddr/models.rb
+++ lib/ddr/models.rb
@@ -1,11 +1,6 @@
 require 'ddr/models/engine'
 require 'ddr/models/version'
-
-# Awful hack to make Hydra::AccessControls::Permissions accessible
-$: << Gem.loaded_specs['hydra-access-controls'].full_gem_path + "/app/models/concerns"
-
 require 'active_record'
-
 require 'hydra-core'
 require 'hydra/validations'
 
