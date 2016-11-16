12fe90325ebab50214ba66984749a8b54526be32
David Chandek-Stark <dchandekstark@gmail.com>
2016-11-16 11:45:08 -0500

    Removed redundant code related to sign out.
    After sign out redirection on servers where Shib authn is required is
    handled in Apache conf.
    Removed override of `Users::SessionsController#after_sign_out_path_for`
    and `Ddr::Auth.sso_logout_url` accessor.
    
    Upgrade note: Remove references to `Ddr::Auth.sso_logout_url` in
    local files (e.g., production.rb) if necessary.

--- lib/ddr/auth.rb
+++ lib/ddr/auth.rb
@@ -91,10 +91,6 @@ module Ddr
       false
     end
 
-    mattr_accessor :sso_logout_url do
-      "/Shibboleth.sso/Logout?return=https://shib.oit.duke.edu/cgi-bin/logout.pl"
-    end
-
     # Grouper gateway implementation
     mattr_accessor :grouper_gateway do
       GrouperGateway
