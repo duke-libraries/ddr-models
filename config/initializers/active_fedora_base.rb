37330933ec778cb485004dbdbaa8e03b41447825
Jim Coble <jim.coble@duke.edu>
2017-01-27 11:12:31 -0500

    Fixes bug in derivatives manager; removes residual cruft related to rights metadata.

--- config/initializers/active_fedora_base.rb
+++ config/initializers/active_fedora_base.rb
@@ -37,17 +37,20 @@ module ActiveFedora
       governable? && admin_policy.present?
     end
 
-    def has_rights_metadata?
-      ds = self.datastreams[Ddr::Datastreams::RIGHTS_METADATA]
-      ds && ds.size && ds.size > 0
-    end
-
     def can_have_struct_metadata?
       datastreams.include? Ddr::Datastreams::STRUCT_METADATA
     end
 
     def has_struct_metadata?
       can_have_struct_metadata? && structMetadata.has_content?
+    end
+
+    def can_have_mezzanine?
+      datastreams.include? Ddr::Datastreams::MEZZANINE
+    end
+
+    def has_mezzanine?
+      can_have_mezzanine? && datastreams[Ddr::Datastreams::MEZZANINE].has_content?
     end
 
     def can_have_multires_image?
