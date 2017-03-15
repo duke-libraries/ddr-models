652e35606db9bd2ff89da7fcd6edca21fa5b53dd
David Chandek-Stark <dchandekstark@gmail.com>
2017-03-15 08:39:44 -0400

    Finished

--- lib/ddr/datastreams/metadata_datastream.rb
+++ lib/ddr/datastreams/metadata_datastream.rb
@@ -7,20 +7,16 @@ module Ddr
       end
 
       # Returns ActiveTriplesTerm now that this is an RDF datastream
-      def values term
+      def values(term)
         term == :format ? self.format : self.send(term)
       end
 
       # Update term with values
       # Note that empty values (nil or "") are rejected from values array
-      def set_values term, values
-        vals = if values.respond_to?(:to_a)
-                 values.to_a
-               else
-                 Array(values)
-               end
-        vals.map!    { |v| v.to_s.strip }
-        vals.reject! { |v| v.blank? }
+      def set_values(term, values)
+        vals = Array(values)
+               .map { |v| v.to_s.strip }
+               .reject { |v| v.blank? }
         begin
           self.send("#{term}=", vals)
         rescue NoMethodError
@@ -30,7 +26,7 @@ module Ddr
 
       # Add value to term
       # Note that empty value (nil or "") is not added
-      def add_value term, value
+      def add_value(term, value)
         vals = values(term).to_a << value
         set_values(term, vals)
       end
