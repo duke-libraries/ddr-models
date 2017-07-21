c15929745f808dac2ad8b190c02a9da105a9df26
Cory Lown <cory.lown@duke.edu>
2017-07-21 10:43:15 -0400

    Index nested_path for searching

--- lib/ddr/index/fields.rb
+++ lib/ddr/index/fields.rb
@@ -75,6 +75,7 @@ module Ddr::Index
     MEDIUM_FACET                = Field.new :medium_facet, :facetable
     MULTIRES_IMAGE_FILE_PATH    = Field.new :multires_image_file_path, :stored_sortable
     NESTED_PATH                 = Field.new :nested_path, :stored_sortable
+    NESTED_PATH_TEXT            = Field.new :nested_path_text, :searchable
     OBJECT_PROFILE              = Field.new :object_profile, :displayable
     OBJECT_STATE                = Field.new :object_state, :stored_sortable
     OBJECT_CREATE_DATE          = Field.new :system_create, :stored_sortable, type: :date
--- lib/ddr/models/indexing.rb
+++ lib/ddr/models/indexing.rb
@@ -130,6 +130,7 @@ module Ddr::Models
         fields[ADMIN_SET_FACET] = admin_set_facet
         fields[COLLECTION_FACET] = collection_facet
         fields[NESTED_PATH] = nested_path
+        fields[NESTED_PATH_TEXT] = nested_path
         fields[ALL_TEXT] = all_text
       end
       fields
